076fc2b3deeffc4104a13ff8204507c1be965a5d
Tony Alfred <tonyyalfred@gmail.com>
2021-12-04 22:35:11 +0200

    Added tested and working QEI code

--- QEI Template/Template Files/inc/QEI.h
+++ QEI Template/Template Files/inc/QEI.h
@@ -2,26 +2,24 @@
 #include "Template Files/inc/common_macros.h"
 #include "Template Files/inc/debug.h"
 
-#define VEL_INT_FREQ         1           // Macro to store the Interrupt frequency of QEI1
-#define QEI1_PPR             32          // Macro to store the PPR of the QEI1
-#define Wheel_Circumference  0.04714f    // The value currently here is due to using radius of shaft! we need to use radius of the wheel itself.
-                                         // but, in testing we could just use a dummy value, like 20 or something, just to ease teseting, till we measure radius of wheel
-                                         // the value written will be in meters
+// Comment this line if Terminal debugging (Putty) isn't required
+#define DEBUG_QEI
+
+#define VEL_INT_FREQ         0.2f        // Macro to store the Interrupt frequency of QEI1. will be changed according to need of PID.
+#define QEI1_PPR             32          // Macro to store the PPR of the QEI1.
+#define Wheel_Circumference  1.7522787f  // As car's diameter was measured manually to be equal to ... meters,
+                                         // hence, circumference = 2*PI*r = PI*d = ...
 
 /* -----------------------      Global Variables        --------------------- */
-// volatile uint32_t ui32QEI1Vel;        // Variable to store the number of pulses accumulated through Velocity Timer's Period in QE1
-// volatile uint32_t ui32QEI1Pos;        // Variable to store the index  of pulses ranging from 0 to 32, with respect to a certain reference
-// volatile uint32_t ui32QEI1Dis;        // Variable to store the number of pulses accumulated since the Vehicle started moving
 // volatile int32_t   i32QEI1Dir;        // Variable to store the direction of QEI1
-// volatile uint16_t ui16QEI1Rpm;        // Variable to store the RPM of QEI1
 
-volatile uint32_t Pulses_Per_Period;
+volatile uint32_t Pulses_per_period;     // Variable to store the number of pulses accumulated through Velocity Timer's Period in QE1
+volatile uint32_t Total_pulses;          // Variable to store the number of pulses accumulated since the Vehicle started moving
 
 volatile float Distance_per_period;      // Value in meters
 volatile float Total_distance;           // Value in meters
 
 volatile float Velocity_meter_per_second;
-volatile float Velocity_kilometer_per_hour;
 
 /* -----------------------      Function Prototypes     --------------------- */
 void QEI1IntHandler(void);
--- QEI Template/Template Files/src/QEI.c
+++ QEI Template/Template Files/src/QEI.c
@@ -17,10 +17,18 @@ void QEI1_Init (void){
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_QEI1);
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
 
+    // Use this code if we are going to use the two signals.
+    // we might need to do a check at the beginning of the interrupt handler to prevent any PID control to occur
+    // if the car is going in the opposite direction for any reason.
+
     // Configure the PC5, PC6 for QEI signals
-    ROM_GPIOPinTypeQEI(GPIO_PORTC_BASE, GPIO_PIN_5 | GPIO_PIN_6);
+    // ROM_GPIOPinTypeQEI(GPIO_PORTC_BASE, GPIO_PIN_5 | GPIO_PIN_6);
+    // ROM_GPIOPinConfigure(GPIO_PC5_PHA1);
+    // ROM_GPIOPinConfigure(GPIO_PC6_PHB1);
+
+    // Configure the PC5 for QEI signals
+    ROM_GPIOPinTypeQEI(GPIO_PORTC_BASE, GPIO_PIN_5);
     ROM_GPIOPinConfigure(GPIO_PC5_PHA1);
-    ROM_GPIOPinConfigure(GPIO_PC6_PHB1);
 
     // Configure the QEI1 to increment for both PhA and PhB for quadrature input with "QEI1_PPR" PPR
     ROM_QEIConfigure(QEI1_BASE, QEI_CONFIG_CAPTURE_A | QEI_CONFIG_NO_RESET | QEI_CONFIG_QUADRATURE | QEI_CONFIG_NO_SWAP , QEI1_PPR);
@@ -35,8 +43,9 @@ void QEI1_Init (void){
     QEIIntRegister(QEI1_BASE, *QEI1IntHandler_ptr);
     ROM_QEIIntEnable(QEI1_BASE, QEI_INTTIMER);
 
-    // Initializing the variable that represents total distance made by the Wheel Encoder is needed to be done before using the QEI.
-    Total_distance = 0;
+    // Initializing the variable that holds the Number of Encoder Pulses accumulated since the Vehicle started moving
+    // to be zero is needed to be done before using the QEI.
+    Total_pulses = 0;
 
     // Enable the QEI1
     ROM_QEIEnable(QEI1_BASE);
@@ -56,38 +65,43 @@ void QEI1IntHandler(void){
     // Clear the Interrupt that is generated
     ROM_QEIIntClear(QEI1_BASE, ROM_QEIIntStatus(QEI1_BASE, true));
 
-    // Calculate the Number of Quadrature Ticks (Number of Encoder Pulses) in "1 / VEL_INT_FREQ" time period
-    Pulses_Per_Period = ROM_QEIVelocityGet(QEI1_BASE);
-
-    // Update the position reading of the encoder
-    // ui32Qei1Pos = ROM_QEIPositionGet(QEI1_BASE);
-
-    // Update the direction reading of the encoder
-    // i32Qei1Dir = ROM_QEIDirectionGet(QEI1_BASE);
-
-    // Calculate the velocity in RPM
-    // ui16Qei1Rpm = ui32Qei1Vel * VEL_INT_FREQ * 60 / QEI1_PPR;
-
     /*
-     * Calculate the Real Velocity of Vehicle in m/s and km/hr, by dividing the total number of pulses within the timer period (ui32QEI1Vel)
+     * Calculate the Real Velocity of Vehicle in m/s and km/hr, by dividing the total number of pulses within the timer period (Pulses_Per_Period)
      * by the number of pulses that create a full revolution (QEI1_PPR) (Value is encoder specific, and is equal to 32 pulses), in order to
-     * find the number of revolutions spanned and then multiply by Circumference of Wheel, in order to find the total distance covered,
-     * and then dividing by the time period that we gave to the Velocity Timer, but, here we will do that by multiplying with the frequency
-     * of The Velocity Timer instead. (VEL_INT_FREQ)
+     * find the number of revolutions spanned and then multiply by Circumference of Wheel (Wheel_Circumference), in order to find the total distance
+     * covered, and then dividing by the time period that we gave to the Velocity Timer of the QEI, but, here we will do that by multiplying with
+     * the frequency of The Velocity Timer instead. (VEL_INT_FREQ).
      */
 
-    Distance_per_period   =  ((float)(Pulses_Per_Period * Wheel_Circumference)/QEI1_PPR);
-    Total_distance = Total_distance + Distance_per_period;
+    /*
+     * Calculate the Number of Quadrature Ticks (Number of Encoder Pulses) in "1 / VEL_INT_FREQ" time period
+     * Note: QEI_VelocityGet gets the number of edges, hence, we divided by two to get number of pulses only.
+     */
+    Pulses_per_period = (ROM_QEIVelocityGet(QEI1_BASE)/2);
+    Distance_per_period = ((float)(Pulses_per_period * Wheel_Circumference)/QEI1_PPR);
+
+    /*
+     * Calculate the Number of Encoder Pulses accumulated since the Vehicle started moving
+     * and the total distance spanned since the Vehicle started moving.
+     */
+    Total_pulses = Total_pulses + Pulses_per_period;
+    Total_distance = ((float)(Total_pulses * Wheel_Circumference)/QEI1_PPR);
+
     Velocity_meter_per_second   = (Distance_per_period * VEL_INT_FREQ);
 
-//  Velocity_meter_per_second   = ((float)(ui32QEI1Vel * WheelCircumfrence * VEL_INT_FREQ) /QEI1_PPR);
-//  Velocity_kilometer_per_hour = Velocity_meter_per_second * 3.6;
+    #ifndef DEBUG_QEI
+    // here write the code that sends the velocity to the required queue used in PID code.
+    #endif
 
-    UART_sendString (UART0_BASE, "Total Distance in meters:  ");
+    #ifdef DEBUG_QEI
+    UART_sendString (UART0_BASE, "\n\r Number of Pulses per period:  ");
+    UART_sendNumber (Pulses_per_period);
+    UART_sendString (UART0_BASE, "\n\r Total Distance in meters:  ");
     UART_sendNumber (Total_distance);
     UART_sendString (UART0_BASE, "\n\r Velocity in meter/second:  ");
     UART_sendNumber (Velocity_meter_per_second);
     UART_sendString (UART0_BASE, "\n\r Velocity in kilometer/hour:  ");
     UART_sendNumber ((Velocity_meter_per_second * 3.6));
     UART_sendString (UART0_BASE, "---------------------------- \n\r");
+    #endif
 }
